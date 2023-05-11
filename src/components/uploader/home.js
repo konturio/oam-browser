6a127bbfd597b7a0574d740e8e63e8e54edcefab
dqunbp <dqunbp@gmail.com>
2023-05-11 13:33:29 +0300

    Try to fix submit notifications

--- src/components/uploader/home.js
+++ src/components/uploader/home.js
@@ -326,7 +326,7 @@ const UploadHome = createReactClass({
         } else {
           this.setState({ submitting: true, uploadCancelled: false });
 
-          AppActions.clearNotification();
+          // AppActions.clearNotification();
 
           // All is well.
           // SUBMIT DATA.
