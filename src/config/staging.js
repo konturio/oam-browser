e53d879fb94cbbe579102bb1feb04dac128190f5
dqunbp <dqunbp@gmail.com>
2023-05-30 13:25:25 +0300

    .

--- src/config/staging.js
+++ src/config/staging.js
@@ -1,8 +1,9 @@
 module.exports = {
+  awsRegion: "us-east-1",
   feedbackSubmissionURL:
     "https://getsimpleform.com/messages/ajax?form_api_token=506fc2ac58582416b6086a68a343e344",
   catalog: {
-    url: "https://api.openaerialmap.org"
+    url: "https://api-staging.openaerialmap.org"
   },
   environment: "staging",
   uploadBucket: "oam-uploader-staging-temp",
@@ -10,9 +11,13 @@ module.exports = {
     "36015894456-3d5ka80qtpaqcjhco3lsl38s1fj0dr71.apps.googleusercontent.com",
   googleDeveloperKey: "",
   googleAppId: "",
-  OAMBrowserUrl: "http://map-staging.openaerialmap.org/",
-  awsKey: "AKIAI4XOYETOVGTNP5HA",
+  OAMBrowserUrl: "https://map-staging.openaerialmap.org/",
+  awsKey: "AKIAZYDVV4ILMV4K5IGF",
   map: {
+    mapbox: {
+      accessToken:
+        "pk.eyJ1Ijoib3BlbmFlcmlhbG1hcCIsImEiOiJjaXl4MjM5c20wMDBmMzNucnZtbnYwZTcxIn0.IKG5flWCS6QfpO3iOdRveg"
+    },
     initialZoom: 3,
     minZoom: 2,
     maxZoom: 18,
@@ -25,7 +30,7 @@ module.exports = {
       {
         id: "osm",
         name: "OpenStreetMap (Standard)",
-        url: "http://a.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        url: "https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"
       }
     ]
   }
